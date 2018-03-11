fa9c92e1223d4de5b94bf54050a1a26e74bcddd9
geoffthemedio <geoffthemedio@gmail.com>
2018-03-11 21:52:42 +0100

    -Added Refresh functions to GovernmentWnd classes. -Debug output

--- UI/MapWnd.cpp
+++ UI/MapWnd.cpp
@@ -2911,6 +2911,9 @@ void MapWnd::InitTurn() {
     for (auto& entry : Empires())
         entry.second->UpdateResourcePools();
 
+    timer.EnterSection("refresh government");
+    m_government_wnd->Refresh();
+
 
     timer.EnterSection("refresh research");
     m_research_wnd->Refresh();
